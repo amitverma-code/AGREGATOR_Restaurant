46d90da25803b2df4072abe8ec02f11247ebc957
amitk <amitk@192.168.0.105>
2020-05-07 22:50:36 +0530

    basic

---
+++ RestaurantAggregator-1/src/main/java/com/agg/RestaurantAggregator1Application.java
@@ -0,0 +1,13 @@
+package com.agg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAggregator1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantAggregator1Application.class, args);
+	}
+
+}
---
+++ RestaurantAggregator-1/src/test/java/com/agg/RestaurantAggregator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAggregator1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
